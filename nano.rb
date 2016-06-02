56bd20ab2b6a10589cee624cca61c4ccebe15d14
tmaone <tmaone@users.noreply.github.com>
2016-06-02 05:36:45 +0200

    use HTTP instead of git

--- nano.rb
+++ nano.rb
@@ -13,7 +13,7 @@ class Nano < Formula
   end
 
   head do
-    url "git://git.savannah.gnu.org/nano.git"
+    url "http://git.savannah.gnu.org/r/nano.git"
     depends_on "automake" => :build
     depends_on "autoconf" => :build
   end
